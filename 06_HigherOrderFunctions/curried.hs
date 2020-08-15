a1bdd0513d8d46fb141676f8d4ed05955889a087
Kenichi SHIRAKURA <surakura@hotmail.co.jp>
2020-08-15 21:45:15 +0900

    examples of higher-order function

---
+++ 06_HigherOrderFunctions/curried.hs
@@ -0,0 +1,11 @@
+multThree :: (Num a) => a -> a -> a -> a
+multThree x y z = x * y * z
+
+compareWithHundred :: (Num a, Ord a) => a -> Ordering
+compareWithHundred = compare 100
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A' .. 'Z'])
---
+++ 06_HigherOrderFunctions/orderism.hs
@@ -0,0 +1,11 @@
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+-- why error?
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f y x = f x y
