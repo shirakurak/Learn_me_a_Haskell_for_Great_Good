bffcef6853d7ab96fc947b52c3a5d6d94dc3e13d
Kenichi SHIRAKURA <surakura@hotmail.co.jp>
2020-08-20 08:30:11 +0900

    asscociation list and findKey function

---
+++ 07_Modules/0017_DataMap.hs
@@ -0,0 +1,18 @@
+-- association list(dictionary)
+let phoneBook = [("betty", "555-2938"),("bonnie", "452-2928"),("patsy", "493-2928"),("lucille", "205-2928"),("wendy", "939-8282"),("penny", "853-2492")]
+
+-- look up some value given a key
+findKey :: (Eq k) => k -> [(k,v)] -> v
+findKey key xs = snd . head . filter (\(k,v) -> key == k) $ xs
+
+-- because not finding the key
+findKey2 :: (Eq k) => k -> [(k,v)] -> Maybe v
+findKey2 key [] = Nothing
+findKey2 key ((k,v):xs) = if key == k
+                            then Just v
+                            else findKey key xs
+
+-- implemented as a fold
+findKey3 :: (Eq k) => k -> [(k,v)] -> Maybe v
+findKey3 key = foldr (\(k,v) acc -> if key == k then Just v else acc) Nothing
+
