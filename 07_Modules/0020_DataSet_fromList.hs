ed6c0d8f25f16b84cc8d72bc41dd341c3ee76ff2
Kenichi SHIRAKURA <surakura@hotmail.co.jp>
2020-08-21 22:18:06 +0900

    Geometry.hs

--- 07_Modules/0020_DataSet_fromList.hs
+++ 07_Modules/0020_DataSet_fromList.hs
@@ -1,3 +1,4 @@
 import qualified Data.Set as Set
 
-text1 = "I just"
+text1 = "I just had an anime dream. Anime... Reality... Are they so different?"
+text2 = "The old man left his garbage can out and now his trash is all over my lawn!"
