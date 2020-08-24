696e38c6e3857b806349a00cd8c402b4ff15b850
Kenichi SHIRAKURA <surakura@hotmail.co.jp>
2020-08-24 22:15:45 +0900

    make type synonyms

---
+++ 08_Making/0029_phonebook.hs
@@ -0,0 +1,19 @@
+phoneBook :: [(String, String)]
+phoneBook = 
+    [("betty","555-2938")
+    ,("bonnie","452-2928")
+    ,("patsy","493-2928")
+    ,("lucille","205-2928")
+    ,("wendy","939-8282")
+    ,("penny","853-2492")
+    ]
+
+-- make a type synonym
+-- type PhoneBook = [(String, String)]
+
+type PhoneNumber = String
+type Name = String
+type PhoneBook = [(Name,PhoneNumber)]
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+inPhoneBook name pnumber pbook = (name,pnumber) `elem` pbook
